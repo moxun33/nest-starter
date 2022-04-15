f5d622df99f75ba91ab15478f2315b5cc65886bb
moxx <moxx@tj-factoring.com>
2022-04-15 16:43:26 +0800

    fix bugs

--- src/core/log/ReportLogger.ts
+++ src/core/log/ReportLogger.ts
@@ -2,31 +2,31 @@ import { ConsoleLogger } from '@nestjs/common';
 
 export class ReportLogger extends ConsoleLogger {
   verbose(message: string) {
-    console.log('【Verbose】', message);
+   // console.log('【Verbose】', message);
     // eslint-disable-next-line prefer-rest-params
     super.verbose.apply(this, arguments);
   }
 
   log(message: string) {
-    console.log('【Log】', message);
+   // console.log('【Log】', message);
     // eslint-disable-next-line prefer-rest-params
     super.log.apply(this, arguments);
   }
 
   debug(message: string) {
-    console.log('【Debug】', message);
+   // console.log('【Debug】', message);
     // eslint-disable-next-line prefer-rest-params
     super.debug.apply(this, arguments);
   }
 
   warn(message: string) {
-    console.log('【Warn】', message);
+  //  console.log('【Warn】', message);
     // eslint-disable-next-line prefer-rest-params
     super.warn.apply(this, arguments);
   }
 
   error(message: string) {
-    console.error('【Error】', message);
+   // console.error('【Error】', message);
     // eslint-disable-next-line prefer-rest-params
     super.error.apply(this, arguments);
   }
